57a54315e87b937aa59faa5c1dbcd344687b9be5
Szymon Misztal <sz.misztal@gmail.com>
2023-03-02 12:46:10 +0100

    add show function

--- warehouse_manager.py
+++ warehouse_manager.py
@@ -1,13 +1,33 @@
 import sys
 
+items = {
+    'Milk': [120, 'l', 2.3],
+    'Sugar': [1000, 'kg', 3],
+    'Flour': [12000, 'kg', 1.2],
+    'Coffee': [25, 'kg', 40]
+}
+
+def get_items():
+    print("Name\tQuantity\tUnit\tUnit Price (PLN)")
+    print('-----------------------------------------------------------')
+    print('Milk','\t', items['Milk'][0], '\t' * 2, items['Milk'][1], '\t', items['Milk'][2])
+    print('Sugar', '\t', items['Sugar'][0], '\t' *2, items['Sugar'][1], '\t', items['Sugar'][2])
+    print('Flour', '\t', items['Flour'][0], '\t' * 2, items['Flour'][1], '\t', items['Flour'][2])
+    print('Coffee', '\t', items['Coffee'][0], '\t' *2, items['Coffee'][1], '\t', items['Coffee'][2])
+    return
+
 def end():
     return print("Bye !"), exit()
 
 while True:
-    menu = input("What would you do ?: \n 1 - Exit \n").lower()
-    if menu not in ('Exit').lower():
+    menu = input("What would you do ?: \n1 - Show \n2 - Exit \n")
+    if menu not in ['Show', 'Exit']:
         print("Please choose another option.")
         continue
-    elif menu == 'exit' and 'Exit':
+    
+    elif menu == 'Show':
+        get_items()
+    
+    elif menu == 'Exit':
         end()
 
